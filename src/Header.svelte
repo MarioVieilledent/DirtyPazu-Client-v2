307826d8454dcaa14c3cc775644b4a409028ed9a
Mario Vieilledent <mario.vieilledent@gmail.com>
2023-06-08 15:07:49 +0200

    Bug fix, prod

--- src/Header.svelte
+++ src/Header.svelte
@@ -11,7 +11,7 @@
     const ulrSvelte = 'https://discord.com/api/oauth2/authorize?client_id=1115564047048392754&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fauth&response_type=code&scope=identify%20email'; 
 
   function login() {
-    window.location.href = ulrSvelte;
+    window.location.href = urlProd;
   }
 </script>
 
