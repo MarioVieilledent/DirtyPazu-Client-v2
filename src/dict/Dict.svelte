f3dea4bb8682a388fcba5b621f37e871ae172a0f
Mario Vieilledent <mario.vieilledent@gmail.com>
2023-06-12 11:17:36 +0200

    Début suggestion

--- src/dict/Dict.svelte
+++ src/dict/Dict.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import Word from "./Word.svelte";
-    import { type DibiWord } from "../types";
-    import { words, fetchingWords } from "../dictionary";
+    import { type DibiWord, type FilteringOption } from "../types";
+    import { fetchingWords } from "../dictionary";
+    import { filterDict, sortDict } from "./dictFunctions";
 
     // Filtered words with searching tools
     let filteredWords: DibiWord[] = [];
-    let filteringOption = {
+    let filteringOption: FilteringOption = {
         dibi: true,
         french: true,
         english: true,
@@ -43,38 +44,7 @@
 
     // Filter words taking in account filtering options
     const filter = () => {
-        let lowerCaseSearch: RegExp = new RegExp(search.toLowerCase());
-        filteredWords = [];
-        words.forEach((word) => {
-            let fits = false;
-            if (
-                filteringOption.dibi &&
-                lowerCaseSearch.test(word.dibi.toLowerCase())
-            ) {
-                fits = true;
-            }
-            if (
-                filteringOption.french &&
-                lowerCaseSearch.test(word.french.toLowerCase())
-            ) {
-                fits = true;
-            }
-            if (
-                filteringOption.english &&
-                lowerCaseSearch.test(word.english.toLowerCase())
-            ) {
-                fits = true;
-            }
-            if (
-                filteringOption.author &&
-                lowerCaseSearch.test(word.author.toLowerCase())
-            ) {
-                fits = true;
-            }
-            if (fits) {
-                filteredWords.push(word);
-            }
-        });
+        filteredWords = filterDict(filteringOption, search);
         sort();
         getWordsToDisplay();
     };
@@ -86,69 +56,7 @@
         } else {
             sortBy = 'date';
         }
-        filteredWords.sort((a: DibiWord, b: DibiWord) => {
-            let diff: number;
-            switch (sortBy) {
-                case "relevance":
-                    diff = relevance(a, b);
-                    break;
-                case "dibi":
-                    diff = a.dibi.localeCompare(b.dibi);
-                    break;
-                case "french":
-                    diff = a.french.localeCompare(b.french);
-                    break;
-                case "date":
-                    diff = a.date.localeCompare(b.date);
-                    break;
-            }
-            if (sortOrder === "asc") {
-                return diff;
-            } else {
-                return -diff;
-            }
-        });
-    }
-
-    // Rework this shitty function
-    function relevance(a: DibiWord, b: DibiWord): number {
-        let expression = search.toLowerCase();
-
-        if (expression === a.dibi.toLowerCase() ||expression === a.french.toLowerCase() ||expression === a.english.toLowerCase()) {
-            return 1000;
-        }
-        
-        if (expression === b.dibi.toLowerCase() ||expression === b.french.toLowerCase() ||expression === b.english.toLowerCase()) {
-            return -1000;
-        }
-
-        let aScore = 0;
-        let bScore = 0;
-
-        for (let length = 1; length <= expression.length; length++) {
-            for (let pos = 0; pos < expression.length - length; pos++) {
-                if (expression.slice(pos, pos+length) === a.dibi.toLowerCase()) {
-                    aScore += length * length;
-                }
-                if (expression.slice(pos, pos+length) === b.dibi.toLowerCase()) {
-                    bScore += length * length;
-                }
-                if (expression.slice(pos, pos+length) === a.french.toLowerCase()) {
-                    aScore += length * length;
-                }
-                if (expression.slice(pos, pos+length) === b.french.toLowerCase()) {
-                    bScore += length * length;
-                }
-                if (expression.slice(pos, pos+length) === a.english.toLowerCase()) {
-                    aScore += length * length;
-                }
-                if (expression.slice(pos, pos+length) === b.english.toLowerCase()) {
-                    bScore += length * length;
-                }
-            }
-        }
-
-        return bScore- aScore;
+        filteredWords = sortDict(filteredWords, sortBy, sortOrder, search);
     }
 
     // Once words filtered and sorted, take a bunch of them to display
@@ -265,7 +173,7 @@
         .list {
             width: 100%;
             height: calc(100% - $simple-search-height);
-            overflow-y: auto;
+            overflow: auto;
         }
     }
 </style>
--- src/suggest/Suggest.svelte
+++ src/suggest/Suggest.svelte
@@ -1,15 +1,208 @@
 <script lang="ts">
-    import { discordConnected } from "../discord";
+    import Word from "../dict/Word.svelte";
+    import { filterDict, sortDict } from "../dict/dictFunctions";
+    import type { Suggestion } from "../dictionary";
+    import { discordConnected, user } from "../discord";
+    import { partsOfSpeech, type DibiWord } from "../types";
 
+    let suggestion: Suggestion = {
+        state: "suggested",
+        versions: [
+            {
+                versionNumber: 1,
+                words: [],
+                upVote: [],
+                downVote: [],
+                thread: [],
+            },
+        ],
+        author: $user.discord.email,
+    };
+    addWord();
+
+    let closeWords: any = [];
+
+    function addWord(): void {
+        suggestion.versions[0].words.push({
+            author: $user.discord.username,
+            dibi: "",
+            partOfSpeech: "Noun",
+            french: "",
+            english: "",
+            description: "",
+            date: Date.now().toString(),
+        });
+        suggestion.versions[0].words = suggestion.versions[0].words;
+    }
+
+    function removeWord(index: number): void {
+        if (suggestion.versions[0].words.length > 1) {
+            suggestion.versions[0].words.splice(index, 1);
+            suggestion.versions[0].words = suggestion.versions[0].words;
+        }
+    }
+
+    function loadCloseWords(): void {
+        closeWords = [];
+        setTimeout(() => {
+            suggestion.versions[0].words.forEach((word) => {
+                loadCloseDibiWords(word.dibi);
+                loadCloseFrenchWords(word.french);
+            });
+        }, 100);
+    }
+
+    function loadCloseDibiWords(expr: any): void {
+        if (expr) {
+            const dibiWordWanted: string = `^${expr}$`;
+            let closeDibiWords: DibiWord[] = [];
+            closeDibiWords = filterDict(
+                { dibi: true, author: false, french: false, english: false },
+                dibiWordWanted
+            );
+            closeDibiWords = sortDict(
+                closeDibiWords,
+                "relevance",
+                "asc",
+                dibiWordWanted
+            );
+            closeWords = [...closeWords, ...closeDibiWords.slice(0, 10)];
+        }
+    }
+
+    function loadCloseFrenchWords(expr: any): void {
+        if (expr) {
+            const dibiWordWanted: string = `${expr}`;
+            let closeFrenchWords: DibiWord[] = [];
+            closeFrenchWords = filterDict(
+                { dibi: false, author: false, french: true, english: false },
+                dibiWordWanted
+            );
+            closeFrenchWords = sortDict(
+                closeFrenchWords,
+                "relevance",
+                "asc",
+                dibiWordWanted
+            );
+            closeWords = [...closeWords, ...closeFrenchWords.slice(0, 10)];
+        }
+    }
+
+    function sendSuggestion(): void {}
 </script>
 
-<div class="content">
+<div class="content fc">
     {#if $discordConnected}
-    
+        <h1>Proposition</h1>
+        <span>Proposer un ou plusieurs mots dibi</span>
+        <div class="suggest fc">
+            {#each suggestion.versions[0].words as word, index}
+                <div class="word fc">
+                    <div class="dibi-partOfSpeech f">
+                        <input
+                            placeholder="Dibi"
+                            type="text"
+                            class={word.partOfSpeech}
+                            bind:value={word.dibi}
+                            on:change={() => loadCloseWords()}
+                        />
+                        <select
+                            class={word.partOfSpeech}
+                            bind:value={word.partOfSpeech}
+                        >
+                            {#each partsOfSpeech as pos}
+                                <option class={pos.english} value={pos.english}
+                                    >{pos.french}</option
+                                >
+                            {/each}
+                        </select>
+                    </div>
+                    <div class="fr-en f">
+                        <input
+                            placeholder="Français"
+                            type="text"
+                            bind:value={word.french}
+                            on:change={() => loadCloseWords()}
+                        />
+                        <input
+                            placeholder="Anglais"
+                            type="text"
+                            bind:value={word.english}
+                        />
+                    </div>
+                    <div class="description f">
+                        <textarea
+                            placeholder="Description"
+                            bind:value={word.description}
+                        />
+                    </div>
+                    {#if suggestion.versions[0].words.length > 1}
+                        <button on:click={() => removeWord(index)}
+                            >Remove</button
+                        >
+                    {/if}
+                </div>
+            {/each}
+            <button on:click={() => addWord()}>New word</button>
+            <div class="close-words fc">
+                <div class="separator" />
+                {#if closeWords.length > 0}
+                    {#each closeWords as closeWord}
+                        <Word word={closeWord} />
+                    {/each}
+                    <div class="separator" />
+                {/if}
+            </div>
+            <button on:click={() => sendSuggestion()}>Proposer</button>
+        </div>
     {:else}
         <span>Authentification Discord nécessaire</span>
     {/if}
 </div>
 
 <style lang="scss">
+    .content {
+        width: 100%;
+        height: 100%;
+        padding: 6px;
+        overflow: auto;
+
+        .suggest {
+            align-items: flex-start;
+            width: 100%;
+
+            .word {
+                align-items: center;
+
+                .dibi-partOfSpeech {
+                    width: 100%;
+                }
+
+                .fr-en {
+                    width: 100%;
+                }
+
+                .description {
+                    width: 100%;
+
+                    textarea {
+                        width: 406px;
+                        height: 48px;
+                    }
+                }
+            }
+
+            .close-words {
+                width: 100%;
+            }
+        }
+    }
+
+    input,
+    button,
+    select,
+    textarea {
+        margin: 0px 6px 6px 0px;
+        width: 200px;
+    }
 </style>
