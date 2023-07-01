846994995b6d63c7f17979d4f421a57b64fa7054
Mario <mario.vieilledent@gmail.com>
2023-07-01 15:13:47 +0200

    UI reprise

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -2,26 +2,39 @@
     import { discordConnected } from "./discord";
     import { currentPage, type Page } from "./types";
 
-
     function setPageTo(page: Page): void {
         $currentPage = page;
     }
 </script>
 
-<div class="nav">
-    <button class="block" on:click={() => setPageTo('home')}>
+<div class="nav fc">
+    <button class="block" on:click={() => setPageTo("home")}>
         <span>Menu</span>
     </button>
-    <button class="block" on:click={() => setPageTo('dict')}>
+    <button class="block" on:click={() => setPageTo("dict")}>
         <span>Dictionnaire</span>
     </button>
     {#if $discordConnected}
-    <button class="block" on:click={() => setPageTo('suggest')}>
-        <span>Proposer</span>
-    </button>
+        <button class="block" on:click={() => setPageTo("suggest")}>
+            <span>Proposer</span>
+        </button>
     {/if}
 </div>
 
 <style lang="scss">
+    .nav {
 
+        .block {
+            span {
+                color: #949ba4;
+            }
+        }
+
+        .block:hover {
+            background-color: #36373D;
+            span {
+                color: #dbdee1;
+            }
+        }
+    }
 </style>
--- src/home/Home.svelte
+++ src/home/Home.svelte
@@ -1,7 +1,18 @@
-<h2>Site pour proposer des mots Dibi</h2>
-<ul style="margin-left: 36px;">
-    <li>Fonctionne avec l'authentification Discord</li>
-    <li>Tout les membres peuvent voter</li>
-    <li>Les mots sont automatiquement ajoutés</li>
-</ul>
-<span>Le site et le serveur v2 sont open-source, pour plus de contenu, je cherche des devs.</span>
+<div class="container">
+    <h2>Site pour proposer des mots Dibi</h2>
+    <ul style="margin-left: 36px;">
+        <li>Fonctionne avec l'authentification Discord</li>
+        <li>Tout les membres peuvent voter</li>
+        <li>Les mots sont automatiquement ajoutés</li>
+    </ul>
+    <span
+        >Le site et le serveur v2 sont open-source, pour plus de contenu, je
+        cherche des devs.</span
+    >
+</div>
+
+<style lang="scss">
+    .container {
+        padding: 12px;
+    }
+</style>
